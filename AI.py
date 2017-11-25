c902e26d810076cf6ef2d978a99d4b434d1027b1
Michael Kronovet <michaelkronovet@MacBook-Pro-8.wv.cc.cmu.edu>
2017-11-25 01:08:53 -0500

    Successfully understand TrainingDataImplementation

--- AI.py
+++ AI.py
@@ -33,6 +33,7 @@ import sys
 import string
 
 
+# I get my chess data from http://chessproblem.my-free-games.com/chess/games/Download-PGN.php
 
 #arguments = sys.argv
 #print(arguments)
@@ -48,11 +49,11 @@ print(infile)'''
 
 
 #Read pgn file:
-with open("/Users/michaelkronovet/Desktop/adams_kasparov_2005.pgn") as f:
+with open("/Users/michaelkronovet/Desktop/15-112/AdamsOK.pgn") as f:
     first_game = chess.pgn.read_game(f)
-    #print(first_game)
+#   print(first_game)
     second_game = chess.pgn.read_game(f)
-    #print("THISIISISISISI",second_game)
+#   print("THISIISISISISI",second_game)
 
 #Go to the end of the game and create a chess.Board() from it:
 first_game = first_game.end()
@@ -93,23 +94,23 @@ board = second_game.board()
 print('FEN of the last position of the game: ', board.fen())
 
 #or if you want to loop over all game nodes:
-#while not game.is_end():
-#    node = game.variations[0]
-#    board = game.board() #print the board if you want, to make sure
-#    game = node
+while not second_game.is_end():
+    node = second_game.variations[0]
+    board = second_game.board() #print the board if you want, to make sure
+    game = node
     #Now we have our board ready, load your engine:
-handler = chess.uci.InfoHandler()
-engine = chess.uci.popen_engine('/Users/michaelkronovet/Desktop/15-112/stockfish-8-mac/Mac/stockfish-8-64') #give correct address of your engine here
-engine.info_handlers.append(handler)
+    handler = chess.uci.InfoHandler()
+    engine = chess.uci.popen_engine('/Users/michaelkronovet/Desktop/15-112/stockfish-8-mac/Mac/stockfish-8-64') #give correct address of your engine here
+    engine.info_handlers.append(handler)
 
-#give your position to the engine:
-engine.position(board)
+    #give your position to the engine:
+    engine.position(board)
 
-#Set your evaluation time, in ms:
-evaltime = 1000 #so 5 seconds
-evaluation = engine.go(movetime=evaltime)
+    #Set your evaluation time, in ms:
+    evaltime = 1000 #so 5 seconds
+    evaluation = engine.go(movetime=evaltime)
 
-#print best move, evaluation and mainline:
-print('best move: ', board.san(evaluation[0]))
-print('evaluation value: ', handler.info["score"][1].cp/100.0)
-print('Corresponding line: ', board.variation_san(handler.info["pv"][1]))
+    #print best move, evaluation and mainline:
+    print('best move: ', board.san(evaluation[0]))
+    print('evaluation value: ', handler.info["score"][1].cp/100.0)
+    print('Corresponding line: ', board.variation_san(handler.info["pv"][1]))
